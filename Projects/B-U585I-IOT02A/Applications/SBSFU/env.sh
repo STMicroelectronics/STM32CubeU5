d6aea18c3ce7716cb8ae9fa9c5b27d6c1e11e91a
Paul GOULPIÉ <paul.goulpie@smile.fr>
2025-03-20 11:41:17 +0100

    env: use envdir to retrieve base path

--- Projects/B-U585I-IOT02A/Applications/SBSFU/env.sh
+++ Projects/B-U585I-IOT02A/Applications/SBSFU/env.sh
@@ -2,4 +2,4 @@
 
 stm32programmercli="C:\Program Files\STMicroelectronics\STM32Cube\STM32CubeProgrammer\bin\STM32_Programmer_CLI.exe"
 stm32tpccli="C:\Program Files\STMicroelectronics\STM32Cube\STM32CubeProgrammer\bin\STM32TrustedPackageCreator_CLI.exe"
-imgtool="C:\Program Files\STMicroelectronics\STM32Cube\STM32CubeProgrammer\bin\Utilities\Windows\imgtool.exe"
+imgtool="${envdir}/Middlewares/Third_Party/mcuboot/scripts/imgtool.py"
